089f5036b36f03ba471fdbe540831424de57800f
saurabh asthana <iamsaurabhasthana@gmail.com>
2025-11-07 02:35:14 +0000

    add.py

--- phases/phase-1/add.py
+++ phases/phase-1/add.py
@@ -1,3 +1,4 @@
-num1 = int(input(print("enter 1st number")))
-num2 = int(input(print("enter 2nd number")))
-print(num1+num2)
+num1 = int(input("enter 1st number"))
+num2 = int(input("enter 2nd number"))
+sum = num1 + num2
+print("sum of ", num1, "and" ,num2, "is ",sum )
